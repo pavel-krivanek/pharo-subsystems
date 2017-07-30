7b799492a2d9978b4141a86e529c47820eb2d38d
Pavel Krivanek <pavel.krivanek@gmail.com>
2017-07-30 22:21:57 +0200

    add playground

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,10 @@
 set -e
 
+wget -O- get.pharo.org/vm60| bash
+
 wget https://github.com/guillep/PharoBootstrap/releases/download/v1.1.1/bootstrapImage.zip
 
 unzip bootstrapImage.zip
+
+./pharo-ui Pharo.image st load.st --save --quit
+
